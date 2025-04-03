5c767ff66c39a679a17e3b1841b21d123f33b983
Logan Conrad <loganmconrad@gmail.com>
2025-04-03 18:21:16 -0400

    sound

--- Objects/Bottle/bottle.gd
+++ Objects/Bottle/bottle.gd
@@ -3,6 +3,8 @@ extends RigidBody3D
 var started: bool = false
 var timer: int = 0
 var init_pos_y = 0
+@onready var glass_hit: AudioStreamPlayer3D = $Glass_Hit
+@onready var glass_shatter: AudioStreamPlayer3D = $Glass_Shatter
 
 func _ready():
 	init_pos_y = position.y
@@ -34,11 +36,17 @@ func clicked():
 	elif global_position.x < -3.5:
 		cap_lower = 0
 
+	play_glass_hit()
 	set_linear_velocity(Vector3(randf_range(cap_lower, cap_higher), randf_range(4.0, 6.0), 0))
 
+func play_glass_hit():
+	glass_hit.pitch_scale = randf_range(1.0, 1.2)
+	glass_hit.play()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if started:
 		Globals.update_high_score()
 		Globals.game_over = true
+		glass_shatter.play()
+		glass_shatter.reparent(get_tree().current_scene)
 		queue_free()
